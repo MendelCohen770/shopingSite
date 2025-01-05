62343ea6c21b7f8a42cd3bbc85d9dc07c7291400
MendelCohen770 <mendeldinacohen@gmail.com>
2025-01-05 22:19:33 +0200

    a

--- Shoping-Site-beckend/Shoping-Site-beckend/obj/Debug/net8.0/Shoping-Site-beckend.AssemblyInfo.cs
+++ Shoping-Site-beckend/Shoping-Site-beckend/obj/Debug/net8.0/Shoping-Site-beckend.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Shoping-Site-beckend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e689077784a3b549d3421317f18a60b85b619c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8798da8cf735370d48e1164ac5e240904e22e4d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Shoping-Site-beckend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Shoping-Site-beckend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
